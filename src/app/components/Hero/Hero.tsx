ba93e04dbff8d46bb70063bc1254b722b3c7eca6
Andres Camilo Ocampo Ramirez <andres_ocampo82211@elpoli.edu.co>
2024-11-25 19:41:52 -0500

    Chore ( Responsive ): Implement main responsive (main)

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -25,7 +25,7 @@ export const PageHero = ({action}:HeroM): ReactElement => {
             hBg="linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(10,26,15,1) 35%, rgba(0,212,255,1) 100%)"  
             padding="10px 20px"          
             borderRadius="10px"            
-            onClick={action}
+            onClick={action}            
           />
         </div>
       </div>
